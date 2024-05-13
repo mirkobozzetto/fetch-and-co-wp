4bd7a834c98deac0e7e56b290e7fca6460c3163c
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-13 11:33:17 +0200

    move files

---
+++ app/public/wp-content/themes/fetch-and-co-fij/articles.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Articles
+ */
+; ?>
+
+<?php
+get_header();
+get_template_part('loop-articles');
+get_footer();
+?>
---
+++ app/public/wp-content/themes/fetch-and-co-fij/movies.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Moovies
+ */
+; ?>
+
+<?php
+get_header();
+get_template_part('loop-moovies');
+get_footer();
+?>
