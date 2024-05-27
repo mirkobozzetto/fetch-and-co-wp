37f96c62d280b01f6a987d850ae59ac3529f81eb
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-27 12:36:37 +0200

    chore: Update AJAX request in main.js
    This commit updates the AJAX request in the main.js file to handle the response correctly. It now checks if the response is not empty before parsing it as JSON. This improves the reliability and error handling of the fetch-and-co-fij theme.

--- app/public/wp-content/themes/fetch-and-co-fij/src/main.js
+++ app/public/wp-content/themes/fetch-and-co-fij/src/main.js
@@ -13,10 +13,16 @@ function loadAjaxTxt() {
 function loadAjaxJson() {
   var charge = new XMLHttpRequest();
   charge.onreadystatechange = function () {
-    let text = JSON.parse(this.responseText);
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("titre").innerHTML =
-        text.pays + " " + text.capitale;
+      if (this.responseText) {
+        try {
+          let text = JSON.parse(this.responseText);
+          document.getElementById("titre").innerHTML =
+            text.pays + " " + text.capitale;
+        } catch (e) {
+          console.error("Erreur lors de l'analyse du JSON : ", e);
+        }
+      }
     }
   };
   charge.open("GET", mainParams.chemin + "/data.json", true);
