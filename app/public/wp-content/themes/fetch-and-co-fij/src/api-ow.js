44aed5e8a52a9950cf31e4f23284b4526dcfd4f0
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-31 12:35:36 +0200

    chore: Update API request in api-ow.js
    This commit updates the API request in the api-ow.js file to use the `$.get` method instead of `$.post`. This change aligns with the intended functionality of making a GET request to retrieve weather data.

--- app/public/wp-content/themes/fetch-and-co-fij/src/api-ow.js
+++ app/public/wp-content/themes/fetch-and-co-fij/src/api-ow.js
@@ -3,18 +3,8 @@ const apiKey = "11cfbee9deadcdf07ef5c0bd56714e2b";
 function ow_api(lat, lon) {
   const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr`;
 
-  $.post(url, function (data, status) {
-    var infos = JSON.parse(data);
-    console.log(infos);
-    // Vous pouvez maintenant utiliser les informations dans `infos` pour afficher les données météorologiques
-    // Par exemple, pour afficher la température actuelle :
-    console.log(`Température actuelle : ${infos.main.temp}°C`);
-    // Pour afficher la description de la météo :
-    console.log(`Description de la météo : ${infos.weather[0].description}`);
-    // Pour obtenir le code de la condition météorologique :
-    const weatherCode = infos.weather[0].id;
-    console.log(`Code de la condition météorologique : ${weatherCode}`);
-    // Vous pouvez maintenant utiliser ce code pour afficher le pictogramme correspondant
+  $.get(url, function (data) {
+    console.log(data);
   });
 }
 
